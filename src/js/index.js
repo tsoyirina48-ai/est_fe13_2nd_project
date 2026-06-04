5b433f269ee2c4c9da4ac3b3948a929078a487e2
Gunnwook Ahn <agw76638@protonmail.com>
2026-06-04 10:03:44 +0900

    feat: 클릭시 탭 패널 보여주기

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,32 @@
 import "../css/pages/index.css";
+
+const tabsContianer = document.querySelector(".tabs__container");
+const tabsList = tabsContianer.querySelector(".tabs__list");
+const tabButtons = tabsList.querySelectorAll(".tab__buttons");
+const tabPanels = tabsContianer.querySelectorAll(".tabs__panels > div");
+
+tabButtons.forEach((tabButtons, index) => {
+  if (index === 0) {
+  } else {
+    tabPanels[index].setAttribute("hidden", "");
+  }
+});
+
+tabsContianer.addEventListener("click", e => {
+  const clickedTab = e.target.closest("a");
+  if (!clickedTab) return;
+
+  e.preventDefault();
+
+  switchTab(clickedTab);
+});
+
+function switchTab(newTab) {
+  const activePanelId = newTab.getAttribute("href");
+  const activePanel = tabsContianer.querySelector(activePanelId);
+
+  tabPanels.forEach(panel => {
+    panel.setAttribute("hidden", "");
+  });
+  activePanel.removeAttribute("hidden");
+}
